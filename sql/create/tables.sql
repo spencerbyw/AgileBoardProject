f6e49fcd5259530475396de0788f9ec1f495e53e
Spencer A. Bywater <spencer.bywater@gmail.com>
2016-10-28 20:09:45 -0700

    Preliminary creates for entities.

--- sql/create/tables.sql
+++ sql/create/tables.sql
@@ -1,34 +1,57 @@
 -- Entities
 create table Board(
+  title varchar(50) not null,
+  description text,
+  primary key(title)
 
 );
 --
 create table Team(
-
+  name varchar(50) not null,
+  primary key(name)
 );
 --
 create table Category(
-
+  id serial,
+  title varchar(50),
+  description text,
+  primary key(id)
 );
 --
 create table Card(
-
+  id serial,
+  due_date date,
+  priority varchar(5),
+  description text,
+  title varchar(50),
+  primary key(id)
 );
 --
 create table TeamMember(
-
+  name varchar(50),
+  email text,
+  role varchar(50),
+  password text,
+  hireDate date,
+  primary key(email)
 );
 --
 create table SeniorDev(
-
+  email text not null references TeamMember(email),
+  dateBecameSenior date,
+  primary key(email)
 );
 --
 create table Intern(
-
+  email text not null references TeamMember(email),
+  school varchar(50),
+  primary key(email)
 );
 --
 create table JuniorDev(
-
+  email text not null references TeamMember(email),
+  almaMater varchar(50),
+  primary key(email)
 );
 
 -- Relationships
--- sql/queries/SQL.sql
+++ sql/queries/SQL.sql
@@ -17,7 +17,7 @@ INSERT INTO teamComposedOf(email,teamName) VALUES("caleb@email.com","team 1");
 INSERT INTO teamComposedOf(email,teamName) VALUES("alex@email.com","team 1");
 
 /*Board */
-INSERT INTO board(title,description) VALUES ("test board","This board is a test board.");
+INSERT INTO Board(title,description) VALUES ("test board","This board is a test board.");
 
 /*Board team relation */
 INSERT INTO boardRunBy(title,teamName) VALUES("test board", "team 1");
@@ -54,16 +54,16 @@ INSERT INTO assignedTo (cardId,email) VALUES (2,"caleb@email");
 
 /*When a team member logs in, select the boards that they are associated with */
 SELECT * FROM Board b,
-runBy rb, Team t, teamComposedOf tco, teamMember tm 
+runBy rb, Team t, teamComposedOf tco, teamMember tm
 WHERE b.title = rb.title
 AND t.name = rb.name
-AND tco.teamName = t.name 
+AND tco.teamName = t.name
 AND tco.email = tm.email
 AND tm.email = "caleb@email.com";
 
 
 /*Load categories and cards for the board when that user selects a board to view*/
-SELECT * FROM Category AS cat 
+SELECT * FROM Category AS cat
 JOIN boardContains AS bc ON bc.categoryId = cat.id
 JOIN Board AS b ON board.title = bc.title
 categorizedAs AS ca ON ca.categoryId = cat.id
@@ -71,36 +71,36 @@ JOIN Card AS card ON card.Id = ca.cardId
 WHERE 1;
 
 /* count high priority cards by team member */
-SELECT tm.name, count(*) AS numCards 
+SELECT tm.name, count(*) AS numCards
 FROM teamMember AS tm, assignedTo AS ato, card AS c
 WHERE tm.email = ato.email AND c.id = ato.id
 AND c.priority = "HIGH"
-GROUP BY tm.name;   
+GROUP BY tm.name;
 
 /*count past due cards per team member */
-SELECT tm.name, count(*) AS numCards 
+SELECT tm.name, count(*) AS numCards
 FROM teamMember AS tm, assignedTo AS ato, card AS c
 WHERE tm.email = ato.email AND c.id = ato.id
 AND c.due_date < GETDATE()
-GROUP BY tm.name; 
+GROUP BY tm.name;
 
 /* past due cards in "to do" category*/
-SELECT * FROM Cards c, categorizedAs ca, Category cat 
-WHERE c.id = ca.cardId 
+SELECT * FROM Cards c, categorizedAs ca, Category cat
+WHERE c.id = ca.cardId
 AND ca.categoryId = cat.categoryId
 AND c.due_date < GETDATE()
 AND cat.title = "to do";
 
 /*Cards that are high priority that are backlogged */
 SELECT * FROM Cards c, isBackloggedOn bl, Board b
-WHERE c.id = bl.cardId 
+WHERE c.id = bl.cardId
 AND b.boardTitle = bl.boardTitle
 AND c.priority = "HIGH";
 
 /* Select team mates who are interns */
 SELECT * FROM TeamMember tm, Intern it, Team t, teamComposedOf tco
-WHERE tm.email = tco.email 
-AND tco.teamName = t.name 
+WHERE tm.email = tco.email
+AND tco.teamName = t.name
 AND it.email = tm.email
 WHERE 1;
 
@@ -110,11 +110,7 @@ FROM(
 	SELECT cat.id AS cid, card.id AS cardNumber
 	FROM Category cat, Card card, categorizedAs ca
 	WHERE cat.id = ca.categoryId
-	AND card.id = ca.cardId) tab1, 
+	AND card.id = ca.cardId) tab1,
 Category c
 WHERE c.id = tab1.cid
 GROUP BY c.id
-
-
-
-

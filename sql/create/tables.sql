755538c6d216ce48ebbb21479eb60746495dcef0
Spencer A. Bywater <spencer.bywater@gmail.com>
2016-10-28 21:50:43 -0700

    Ok all the create statements parse correctly 👍

--- sql/create/tables.sql
+++ sql/create/tables.sql
@@ -1,6 +1,6 @@
 -- Entities
 create table Board(
-  title varchar(50) not null on delete cascade,
+  title varchar(50) not null,
   description text,
   primary key(title)
 );
@@ -11,14 +11,14 @@ create table Team(
 );
 --
 create table Category(
-  id serial on delete cascade,
+  id serial,
   title varchar(50),
   description text,
   primary key(id)
 );
 --
 create table Card(
-  id serial on delete cascade,
+  id serial,
   due_date date,
   priority varchar(5),
   description text,
@@ -28,7 +28,7 @@ create table Card(
 --
 create table TeamMember(
   name varchar(50),
-  email text on delete cascade,
+  email text,
   role varchar(50),
   password text,
   hireDate date,
@@ -36,63 +36,80 @@ create table TeamMember(
 );
 --
 create table SeniorDev(
-  email text not null references TeamMember(email) on delete cascade,
+  email text,
   dateBecameSenior date,
-  primary key(email)
+  primary key(email),
+  foreign key (email) references TeamMember (email) on delete cascade
 );
 --
 create table Intern(
-  email text not null references TeamMember(email),
+  email text,
   school varchar(50),
-  primary key(email)
+  primary key(email),
+  foreign key (email) references TeamMember (email) on delete cascade
 );
 --
 create table JuniorDev(
-  email text not null references TeamMember(email),
+  email text,
   almaMater varchar(50),
-  primary key(email)
+  primary key(email),
+  foreign key (email) references TeamMember (email) on delete cascade
 );
 
 -- Relationships
 -- Bridges Board-[]<-Category
 create table contains(
-  board_title varchar(50) references Board(title),
-  category_id serial references Category(id),
-  primary key(board_title, category_id)
+  board_title varchar(50),
+  category_id serial,
+  primary key(board_title, category_id),
+  foreign key (board_title) references Board (title),
+  foreign key (category_id) references Category (id) on delete cascade
 );
 -- Bridges Board->[]<-Card
 create table isBackloggedOn(
-  board_title varchar(50) references Board(title),
-  card_id serial references Card(id),
-  primary key(board_title, card_id)
+  board_title varchar(50),
+  card_id serial,
+  primary key(board_title, card_id),
+  foreign key (board_title) references Board (title),
+  foreign key (card_id) references Card (id)
 );
 -- Bridges Category-[]<-Card
 create table categorizedAs(
  category_id serial references Category(id),
- card_id serial references Card(id)
- primary key(category_id, card_id)
+ card_id serial references Card(id),
+ primary key (category_id, card_id),
+ foreign key (category_id) references Category(id),
+ foreign key (card_id) references Card(id) on delete cascade
 );
 -- Bridges Board->[]<-Team
 create table runBy(
   board_title varchar(50) references Board(title),
-  team_name varchar(50) references Team(name)
-  primary key(board_title, team_name)
+  team_name varchar(50) references Team(name),
+  primary key(board_title, team_name),
+  foreign key (board_title) references Board(title),
+  foreign key (team_name) references Team(name)
 );
 -- Bridges Team->[]<-SeniorDev
 create table hasLeader(
   team_name varchar(50) references Team(name),
   dev_email text references SeniorDev(email),
-  primary key(team_name, dev_email)
+  primary key(team_name, dev_email),
+  foreign key (team_name) references Team(name),
+  foreign key (dev_email) references SeniorDev(email)
 );
 -- Bridges Team-[]<-TeamMember
 create table composedOf(
   team_name varchar(50) references Team(name),
   member_email text references TeamMember(email),
-  primary key(team_name, member_email)
+  primary key(team_name, member_email),
+  foreign key (team_name) references Team(name),
+  foreign key (member_email) references TeamMember(email)
 );
 -- Bridges TeamMember-[]-Card
 create table assignedTo(
   member_email text references TeamMember(email),
   card_id serial references Card(id),
-  primary key(member_email, card_id)
+  primary key(member_email, card_id),
+  foreign key (member_email) references TeamMember(email),
+  foreign key (card_id) references Card(id)
 );

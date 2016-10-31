91fdd148f4b4ca122efef4963663ae80ea257a1e
Spencer A. Bywater <spencer.bywater@gmail.com>
2016-10-30 19:13:22 -0700

    Fixing the queries just a tiny bit.

--- sql/queries/Phase 2 Demo.sql
+++ sql/queries/Phase 2 Demo.sql
@@ -32,7 +32,7 @@ INSERT INTO boardcontains(category_id,board_title) VALUES (21,'test board');
 INSERT INTO boardcontains(category_id,board_title) VALUES (22,'test board');
 INSERT INTO boardcontains(category_id,board_title) VALUES (23,'test board');
 
-INSERT INTO Card(due_date,priority,description,title) VALUES ('2016-11-10','HIGH','this is a test card','Card 0');
+INSERT INTO Card(due_date,priority,description,title) VALUES ('2016-10-10','HIGH','this is a test card','Card 0');
 INSERT INTO Card(due_date,priority,description,title) VALUES ('2016-11-09','MED','this is another test card','Card 1');
 INSERT INTO Card(due_date,priority,description,title) VALUES ('2016-12-01','LOW','a test card','Card 2');
 INSERT INTO Card(due_date,priority,description,title) VALUES ('2016-12-01','LOW','a card that is backlogged','Card 3');
@@ -43,9 +43,10 @@ INSERT INTO categorizedAs(category_id,card_id) VALUES (22,52);
 INSERT INTO categorizedAs(category_id,card_id) VALUES (22,53);
 
 /*backlog a card*/
-INSERT INTO isBackloggedOn(board_title,card_id) VALUES('test board',54)
+INSERT INTO isBackloggedOn(board_title,card_id) VALUES('test board',54);
 
 /*assign cards */
+
 INSERT INTO assignedTo (card_id,member_email) VALUES (51,'caleb@email.com');
 INSERT INTO assignedTo (card_id,member_email) VALUES (52,'alex@email.com');
 INSERT INTO assignedTo (card_id,member_email) VALUES (53,'caleb@email.com');
@@ -54,16 +55,16 @@ INSERT INTO assignedTo (card_id,member_email) VALUES (53,'caleb@email.com');
 
 /*When a team member logs in, select the boards that they are associated with */
 SELECT * FROM Board b,
-runBy rb, Team t, composedOf tco, teamMember tm 
+runBy rb, Team t, composedOf tco, teamMember tm
 WHERE b.title = rb.board_title
 AND t.name = rb.team_name
-AND tco.team_name = t.name 
+AND tco.team_name = t.name
 AND tco.member_email = tm.email
 AND tm.email = 'caleb@email.com';
 
 
 /*Load categories and cards for the board when that user selects a board to view*/
-SELECT * FROM Category AS cat 
+SELECT * FROM Category AS cat
 JOIN boardContains AS bc ON bc.category_id = cat.id
 JOIN Board AS b ON b.title = bc.board_title
 JOIN categorizedAs AS ca ON ca.category_id = cat.id
@@ -71,49 +72,45 @@ JOIN Card AS card ON card.Id = ca.card_id
 WHERE b.title = 'test board';
 
 /* count high priority cards by team member */
-SELECT tm.name, count(*) AS numCards 
+SELECT tm.name, count(*) AS numCards
 FROM teamMember AS tm, assignedTo AS ato, card AS c
 WHERE tm.email = ato.member_email AND c.id = ato.card_id
 AND c.priority = 'HIGH'
-GROUP BY tm.name;   
+GROUP BY tm.name;
 
 /*count past due cards per team member */
-SELECT tm.name, count(*) AS numCards 
+SELECT tm.name, count(*) AS numCards
 FROM teamMember AS tm, assignedTo AS ato, card AS c
 WHERE tm.email = ato.member_email AND c.id = ato.card_id
 AND c.due_date < NOW()
-GROUP BY tm.name; 
+GROUP BY tm.name;
 
 /* past due cards in 'to do' category*/
-SELECT * FROM Card c, categorizedAs ca, Category cat 
-WHERE c.id = ca.card_id 
+SELECT * FROM Card c, categorizedAs ca, Category cat
+WHERE c.id = ca.card_id
 AND ca.category_id = cat.id
 AND c.due_date < NOW()
 AND cat.title = 'to do';
 
 /*Cards that are high priority that are backlogged */
 SELECT * FROM Card c, isBackloggedOn bl, Board b
-WHERE c.id = bl.card_id 
+WHERE c.id = bl.card_id
 AND b.title = bl.board_title
 AND c.priority = 'HIGH';
 
 /* Select team mates who are interns */
 SELECT * FROM TeamMember tm, Intern it, Team t, composedOf tco
-WHERE tm.email = tco.member_email 
-AND tco.team_name = t.name 
+WHERE tm.email = tco.member_email
+AND tco.team_name = t.name
 AND it.email = tm.email;
 
-/* how many cards are in each category for a board */
+/* how many cards are in each category */
 SELECT c.id, COUNT(tab1.cardNumber)
 FROM(
 	SELECT cat.id AS cid, card.id AS cardNumber
 	FROM Category cat, Card card, categorizedAs ca
 	WHERE cat.id = ca.category_id
-	AND card.id = ca.card_id) tab1, 
+	AND card.id = ca.card_id) tab1,
 Category c
 WHERE c.id = tab1.cid
-GROUP BY c.id'
-
-
-
-
+GROUP BY c.id;
